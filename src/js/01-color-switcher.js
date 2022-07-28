ab26d41de0038fbdeb0dc0492a4f1115e6066c2e
Люда <1982luda30@gmail.com>
2022-07-29 00:19:13 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const bodyChangeEl = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+btnStop.setAttribute('disabled', '');
+
+btnStart.addEventListener('click', element => {
+    element.target.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+    
+    timerId = setInterval(() => {
+        bodyChangeEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+btnStop.addEventListener('click', element => {
+    element.target.setAttribute('disabled', true);
+    btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+});
